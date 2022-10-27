607a3dc2b10616f5f00126ea4640dd691622c717
igor <igor@ark-in.info>
2022-10-27 18:33:02 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const authRouter = require("./routes/auth.routes");
 const taskRouter = require("./routes/task.routes");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 app.use("/task", taskRouter);
 app.use("/auth", authRouter);
 
