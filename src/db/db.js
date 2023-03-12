4d49a8fc33fed074c0955060b6a8e65738bc082f
ARSync <63187302+Async-Reaper@users.noreply.github.com>
2023-03-12 20:04:11 +0700

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "postgres",
-  password: "Ikd12213443",
-  host: "localhost",
+  user: "user",
+  password: "kjDJBOx4zQSA8nhbtDD70Y15B5XVgGZ0",
+  host: "dpg-cg6snjt269v5l67c5c40-a",
   port: 5432,
-  database: "todo_list",
+  database: "posts_en5p",
 });
 
 module.exports = pool;
